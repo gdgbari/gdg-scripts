20a645bf7b36c410e0420b07e057974a09009ee1
Federico Carrozzino <federicocarrozzino98@gmail.com>
2024-10-24 09:34:22 +0200

    quizzes utilities

--- devfest_admin_tools/lib/quizzes.js
+++ devfest_admin_tools/lib/quizzes.js
@@ -6,6 +6,9 @@ const firestore = getFirestore();
 module.exports = {
     removeDeprecatedFields,
     deleteAllQuizzes,
+    openAllQuizzes,
+    closeAllQuizzes,
+    getSpecialQuizFromUser
 }
 
 async function removeDeprecatedFields() {
@@ -28,4 +31,51 @@ async function deleteAllQuizzes() {
     await deleteAllDocuments(questionsRef);
     const quizzesRef = firestore.collection('quizzes');
     await deleteAllDocuments(quizzesRef);
+}
+
+async function openAllQuizzes() {
+    const quizzesRef = firestore.collection('quizzes');
+    const snapshot = await quizzesRef.get();
+
+    const batch = firestore.batch();
+    snapshot.forEach(doc => {
+        batch.update(doc.ref, {
+            isOpen: true
+        });
+    });
+
+    await batch.commit();
+}
+
+async function closeAllQuizzes() {
+    const quizzesRef = firestore.collection('quizzes');
+    const snapshot = await quizzesRef.get();
+
+    const batch = firestore.batch();
+    snapshot.forEach(doc => {
+        batch.update(doc.ref, {
+            isOpen: false
+        });
+    });
+
+    await batch.commit();
+}
+
+async function getSpecialQuizFromUser(userId) {
+    const userQuizResults = await firestore.collection('users').doc(userId).collection('quizResults').get();
+    const specialQuizzes = await firestore.collection('quizzes').where('type', '==', 'special').get();
+
+    const specialQuizResults = [];
+    userQuizResults.forEach(
+        quizResult => {
+            specialQuizzes.forEach(specialQuiz => {
+                if (quizResult.data().quizId === specialQuiz.id) {
+                    specialQuizResults.push(quizResult);
+                }
+            }
+            );
+        }
+    );
+
+    return specialQuizResults;
 }
